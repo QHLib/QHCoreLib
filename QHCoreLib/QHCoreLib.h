dee10c35065753492fd08f022c2a41c715b63867
changtang(唐畅) <changtang@tencent.com>
2017-05-17 15:00:07 +0800

    create project QHCoreLib

---
+++ QHCoreLib/QHCoreLib.h
@@ -0,0 +1,13 @@
+//
+//  QHCoreLib.h
+//  QHCoreLib
+//
+//  Created by changtang on 2017/5/17.
+//  Copyright © 2017年 Tencent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QHCoreLib : NSObject
+
+@end
---
+++ QHCoreLib/QHCoreLib.m
@@ -0,0 +1,13 @@
+//
+//  QHCoreLib.m
+//  QHCoreLib
+//
+//  Created by changtang on 2017/5/17.
+//  Copyright © 2017年 Tencent. All rights reserved.
+//
+
+#import "QHCoreLib.h"
+
+@implementation QHCoreLib
+
+@end
