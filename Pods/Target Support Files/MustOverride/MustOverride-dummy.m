0057a8ac9d40b22a4549d47bf991236bc2485a43
Tony Tang <tangchang21@gmail.com>
2017-05-29 14:26:51 +0800

    add Pods to git

---
+++ Pods/Target Support Files/MustOverride/MustOverride-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MustOverride : NSObject
+@end
+@implementation PodsDummy_MustOverride
+@end
---
+++ Pods/Target Support Files/Pods-QHCoreLib/Pods-QHCoreLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QHCoreLib : NSObject
+@end
+@implementation PodsDummy_Pods_QHCoreLib
+@end
