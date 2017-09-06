a4d34385ad3fda03c608c5c2b532400d98deb467
jcluan <1013770844@qq.com>
2017-09-06 18:39:13 +0800

    add UIKit categories

--- QHCoreLib/UI/UIKit+QHCoreLib.h
+++ QHCoreLib/UI/UIKit+QHCoreLib.h
@@ -13,3 +13,34 @@
 + (void)qh_disableAnimationDuringBlock:(dispatch_block_t)block;
 
 @end
+
+@interface UIView (Frame)
+
+@property (nonatomic, assign) CGFloat top;
+@property (nonatomic, assign) CGFloat left;
+@property (nonatomic, assign) CGFloat bottom;
+@property (nonatomic, assign) CGFloat right;
+
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+
+@end
+
+@interface UIColor (Hex)
+
++ (UIColor *)colorWithHex:(NSUInteger)hex;
+
++ (UIColor *)colorWithHex:(NSUInteger)hex alpha:(CGFloat)alpha;
+
+@end
+
+@interface UIImage (Color)
+
++ (UIImage *)imageWithColor:(UIColor *)color;
+
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size;
+
+@end
--- QHCoreLib/UI/UIKit+QHCoreLib.m
+++ QHCoreLib/UI/UIKit+QHCoreLib.m
@@ -20,3 +20,140 @@
 }
 
 @end
+
+@implementation UIView (Frame)
+- (void)setTop:(CGFloat)top
+{
+    CGRect frame = self.frame;
+    frame.origin.y = top;
+    self.frame = frame;
+}
+- (CGFloat)top
+{
+    return self.frame.origin.y;
+}
+
+- (void)setLeft:(CGFloat)left
+{
+    CGRect frame = self.frame;
+    frame.origin.x = left;
+    self.frame = frame;
+}
+- (CGFloat)left
+{
+    return self.frame.origin.x;
+}
+
+- (void)setBottom:(CGFloat)bottom
+{
+    CGRect frame = self.frame;
+    frame.origin.y = bottom - frame.size.height;
+    self.frame = frame;
+}
+- (CGFloat)bottom
+{
+    CGRect frame = self.frame;
+    return frame.origin.y + frame.size.height;
+}
+
+- (void)setRight:(CGFloat)right
+{
+    CGRect frame = self.frame;
+    frame.origin.x = right - frame.size.width;
+    self.frame = frame;
+}
+- (CGFloat)right
+{
+    CGRect frame = self.frame;
+    return frame.origin.x + frame.size.width;
+}
+
+- (void)setWidth:(CGFloat)width
+{
+    CGRect frame = self.frame;
+    frame.size.width = width;
+    self.frame = frame;
+}
+- (CGFloat)width
+{
+    return self.frame.size.width;
+}
+
+- (void)setHeight:(CGFloat)height
+{
+    CGRect frame = self.frame;
+    frame.size.height = height;
+    self.frame = frame;
+}
+- (CGFloat)height
+{
+    return self.frame.size.height;
+}
+
+- (void)setCenterX:(CGFloat)centerX
+{
+    CGPoint center = self.center;
+    center.x = centerX;
+    self.center = center;
+}
+- (CGFloat)centerX
+{
+    return self.center.x;
+}
+
+- (void)setCenterY:(CGFloat)centerY
+{
+    CGPoint center = self.center;
+    center.y = centerY;
+    self.center = center;
+}
+- (CGFloat)centerY
+{
+    return self.center.y;
+}
+
+@end
+
+@implementation UIColor (Hex)
+
++ (UIColor *)colorWithHex:(NSUInteger)hex
+{
+    return [UIColor colorWithHex:hex alpha:1.0f];
+}
+
++ (UIColor *)colorWithHex:(NSUInteger)hex alpha:(CGFloat)alpha
+{
+    NSUInteger red = ((hex & 0xff0000) >> 16);
+    NSUInteger green = ((hex & 0xff00) >> 8);
+    NSUInteger blue = (hex & 0xff);
+    CGFloat r = (CGFloat)red / 255.0f;
+    CGFloat g = (CGFloat)green  / 255.0f;
+    CGFloat b = (CGFloat)blue / 255.0f;
+    
+    return [UIColor colorWithRed:r green:g blue:b alpha:alpha];
+}
+
+@end
+
+@implementation UIImage (Color)
+
++ (UIImage *)imageWithColor:(UIColor *)color
+{
+    return [UIImage imageWithColor:color size:CGSizeMake(1, 1)];
+}
+
++ (UIImage *)imageWithColor:(UIColor *)color size:(CGSize)size
+{
+    CGRect rect = CGRectMake(0,0, size.width, size.height);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    UIImage *theImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return theImage;
+}
+
+@end
+
+
